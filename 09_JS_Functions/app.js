0c1f269ac4d3735d26a074477a812cb2cd902946
LakshmiP10 <lakshmi.uitech@gmail.com>
2024-11-22 13:14:33 +0530

    modified app.js in  09_JS_Functions/app.js

--- 09_JS_Functions/app.js
+++ 09_JS_Functions/app.js
@@ -60,4 +60,67 @@ let sum = function(a,b){
     return result;
 };
 let output = sum(10,20);
-console.log(output)
+console.log(output);
+
+//complex functions
+//A function with object as a parameter
+let printObject = function(obj){
+    console.log(obj);
+}
+printObject({name:"Lakshmi",age:35});
+
+let mobile = {
+    brand:'Apple',
+    price:35000,
+    color:'silver'
+}
+printObject(mobile);
+
+//function with an array as a parameter
+let result = '';
+let printArray = function(array){
+    for(let index in array){
+        result += `Index: ${index} => Value : ${array[index]} \n`;
+    }
+    console.log(result);
+}
+printArray([10,20,30,40]);
+let colors = ['white','black','silver','purple'];
+printArray(colors);
+
+//function inside an object
+let student = {
+    firstName :'Sharavan',
+    lastName : 'Sai',
+    fullName:function(){
+        return `${this.firstName} ${this.lastName}`;
+    }
+    
+};
+console.log(student.fullName());
+
+//nested functions
+let outerFn = function(){
+    console.log('I am an Outer function')
+    let innerFn = function(){
+        console.log('I am an Inner Function');
+    }
+    innerFn();
+}
+outerFn();
+//Twisted functin
+let twisedFn = function(){
+    let name = 'John';
+    let printStudent = function(){
+        let student = {
+            name: 'Lakshmi',
+            age:20,
+            course:'MCA'
+        };
+        return student;
+    };
+    return printStudent;
+};
+let innerFn = twisedFn();
+let studentObj = innerFn();
+console.log(studentObj.name)
